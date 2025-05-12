c2367328805b11846f523106b2424ff292d2d89c
pedrorchagas <perodrigues132006@gmail.com>
2025-05-12 11:00:25 -0300

    add: Lógica da implementação do banco de dados para salvamento das pizzas no banco de dados

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
 import streamlit as st
 import blob_manager
+import sql_manager
 
 st.title("Pizza - Ecommerce")
 st.write("O melhor e-commerce do mundo, aqui só vendemos pizzas!!")
 
 st.title("Pizzas")
-sabores_pizzas = [
-    {"Sabor": "Pepperoni", "Image-Link": "", "Fatias": 15, "Tamanho": "G", "Ingredientes": "Farinha de trigo, queijo, calabresa, oregano"},
-    {"Sabor": "Bacon", "Image-Link": "", "Fatias": 5, "Tamanho": "P", "Ingredientes": "Farinha de trigo, queijo, bacon, oregano"}
-    ]
+sabores_pizzas = sql_manager.get_products()
+if not sabores_pizzas:
+    st.write("Nenhum sabor de pizza encontrado.")
+    st.write("Por favor, adicione um sabor de pizza.")
+else:
+    for pizza in sabores_pizzas:
 
-for pizza in sabores_pizzas:
-    with st.container():
-        st.header(f"Sabor: {pizza["Sabor"]}")
-        st.subheader(f"Ingredientes: {pizza["Ingredientes"]}")
-        #st.image(image=pizza["Image-Link"])
-        st.write(f"Fatias: {pizza["Fatias"]}")
-        st.write(f"Tamanho: {pizza["Tamanho"]}")
+        with st.container():
+            st.header(f"Sabor: {pizza["product_name"]}")
+            st.subheader(f"Ingredientes: {pizza["product_ingredients"]}")
+            st.image(image=pizza["product_image_link"])
+            st.write(f"Fatias: {pizza["product_slices"]}")
+            st.write(f"Tamanho: {pizza["product_slices"]}")
+            if st.button("Deletar", key=pizza["product_id"]):
+                sql_manager.delete_product(pizza["product_id"])
+                st.success("Sabor de pizza deletado com sucesso!")
+                st.rerun()
 
 st.title("Adicionar sabor de pizza")
-new_sabor = st.text_input("Sabor: ")
-new_fatias = st.text_input("Numero de fatias: ")
-new_ingredientes = st.text_input("Ingredientes: ")
-new_imagem = st.file_uploader("Escolha uma imagem", accept_multiple_files=False, type=["jpeg"])
+with st.container():
+    new_sabor = st.text_input("Sabor: ")
+    new_fatias = st.text_input("Numero de fatias: ")
+    new_ingredientes = st.text_input("Ingredientes: ")
+    new_imagem = st.file_uploader("Escolha uma imagem", accept_multiple_files=False, type=["jpeg"])
 
-if st.button("Criar novo sabor de pizza"):
-    blob_manager.upload_image(new_imagem)
+    if st.button("Criar novo sabor de pizza"):
+        image_url = blob_manager.upload_image(new_imagem)
+        sql_manager.insert_product(
+            product_name=new_sabor,
+            product_image_link=image_url,
+            product_ingredients=new_ingredientes,
+            product_slices=int(new_fatias)
+        )
+        st.success("Sabor de pizza adicionado com sucesso!")
+        st.rerun()
 
-    
--- sql_manager.py
+++ sql_manager.py
@@ -8,20 +8,54 @@ SERVER = os.getenv("DATABASE_HOST")
 
 
 
-database = pymssql.connect(server=SERVER, user=os.getenv("DATABASE_USERNAME"), password=os.getenv("DATABASE_PASSWORD"))
 
-cursor = database.cursor()
-cursor.execute(
-    """
-        CREATE TABLE [dbo].[products] (
-            [Id]                  INT  IDENTITY (1, 1) NOT NULL,
-            [product_name]        TEXT NULL,
-            [product_image_link]  TEXT NULL,
-            [product_ingredients] TEXT NULL,
-            [product_slices]      INT  NULL,
-            CONSTRAINT [PK_products] PRIMARY KEY CLUSTERED ([Id] ASC)
-        );
+def open_connection():
+    database = pymssql.connect(server=SERVER, user=os.getenv("DATABASE_USERNAME"), password=os.getenv("DATABASE_PASSWORD"), database=os.getenv("DATABASE_DB"))
+    return database
 
-    """
-)
+def insert_product(product_name, product_image_link, product_ingredients, product_slices):
+    database = open_connection()
+    cursor = database.cursor()
+    cursor.execute(
+        """
+            INSERT INTO dbo.products (product_name, product_image_link, product_ingredients, product_slices)
+            VALUES (%s, %s, %s, %s)
+        """,
+        (product_name, product_image_link, product_ingredients, product_slices)
+    )
+    database.commit()
+    cursor.close()
 
+def get_products():
+    database = open_connection()
+    cursor = database.cursor()
+    cursor.execute(
+        """
+            SELECT * FROM products
+        """
+    )
+    rows = cursor.fetchall()
+    products = []
+    for row in rows:
+        product = {
+            "product_id": row[0],
+            "product_name": row[1],
+            "product_image_link": row[2],
+            "product_ingredients": row[3],
+            "product_slices": row[4]
+        }
+        products.append(product)
+    cursor.close()
+    return products
+
+def delete_product(product_id):
+    database = open_connection()
+    cursor = database.cursor()
+    cursor.execute(
+        """
+            DELETE FROM products WHERE product_id = %s
+        """,
+        (product_id,)
+    )
+    database.commit()
+    cursor.close()
